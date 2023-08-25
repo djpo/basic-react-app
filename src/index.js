92b12d732ad2842dd5592052a1c9227ed193c3cf
Poppe <djpo@users.noreply.github.com>
2023-08-25 15:15:00 +0200

    fix typo disableReactDevtools

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { disableReactDevtools } from '@fvilers/disable-react-devtools';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-if (process.env.NODE_ENV === 'production') disableReactDevtools();
+if (process.env.NODE_ENV === 'production') disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
